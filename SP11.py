9506a3f5115e7b805884bd5ab0e00126d68c6384
XxMasterepicxX0 <56774983+XxMasterepicxX0@users.noreply.github.com>
2022-11-21 15:45:01 -0500

    Update SP11.py

--- SP11.py
+++ SP11.py
@@ -6,7 +6,7 @@ COLOR = (255,255,255)
 FPS = 120
 VEL = 2
 #C stands for character
-C_WIDTH, C_HEIGHT = 55, 40
+C_WIDTH, C_HEIGHT = 60, 100
 CHARACTER = pygame.image.load(os.path.join("Assets", "character.png"))
 CHARACTER = pygame.transform.scale(CHARACTER, (C_WIDTH, C_HEIGHT))
 
@@ -18,18 +18,17 @@ pygame.display.set_caption("Shrek Party 11")
 #players movement
 def movement(key_pressed, player):
         #left
-        if key_pressed[pygame.K_a]:
+        if key_pressed[pygame.K_a] and player.x - VEL > 0:
              player.x -= VEL
         #right
-        if key_pressed[pygame.K_d]:
+        if key_pressed[pygame.K_d] and player.x + VEL + player.width < WIDTH:
              player.x += VEL
         #up
-        if key_pressed[pygame.K_w]:
+        if key_pressed[pygame.K_w] and player.y - VEL > 0 : 
              player.y -= VEL
         #down
-        if key_pressed[pygame.K_s]:
+        if key_pressed[pygame.K_s] and player.y + VEL + player.height < HEIGHT:
              player.y += VEL
-
 
 #draws things to the image
 def draw_window(player):
@@ -53,7 +52,7 @@ def main():
         """
         This should pass the key pressed to the movement function, and the movement function should take that and run it through an if statement.
         However, since the function also uses player, you need to pass player through it. So the function knows who the player is.
-        But that's what I believe is going on; I have no idea what I'm doing. 
+        But that's what I believe is going on; I have no idea what I'm doing.
         """
         key_pressed = pygame.key.get_pressed()
         movement(key_pressed, player)
